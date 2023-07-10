9f418e29c8b309277eee3e3d638af2c3695a657f
Maximo Zaragoza <galardonado7@gmail.com>
2023-07-10 14:53:23 -0300

    fi

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -3,8 +3,9 @@ const uploadController = (req, res) => {
   if (req.file === undefined){
     return res.status(400).json({ message: 'No se ha proveido un archivo'})
   } else {
-    const imageUrl = `http://localhost:5000/images/${req.file.filename}`;
-    const imagePageUrl = `http://localhost:5000/image/${req.file.filename}`;
+    const baseUrl = "https://imageuploadmaxz.onrender.com" ;
+    const imageUrl = `${baseUrl}/images/${req.file.filename}`;
+    const imagePageUrl = `${baseUrl}/image/${req.file.filename}`;
     
   
     res.json({ imageUrl, imagePageUrl });
