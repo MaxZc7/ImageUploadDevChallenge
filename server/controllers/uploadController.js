6d4f9e7d4da0f45fc8db30f34f5a9c793d198122
Maximo Zaragoza <galardonado7@gmail.com>
2023-07-10 15:12:35 -0300

    sss

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -3,7 +3,8 @@ const uploadController = (req, res) => {
   if (req.file === undefined){
     return res.status(400).json({ message: 'No se ha proveido un archivo'})
   } else {
-    const baseUrl = "https://imageuploadmaxz.onrender.com" ;
+    // 'http://localhost:5000' ||
+    const baseUrl =  "https://imageuploadmaxz.onrender.com/";
     const imageUrl = `${baseUrl}/images/${req.file.filename}`;
     const imagePageUrl = `${baseUrl}/image/${req.file.filename}`;
     
--- server/routes/Upload.js
+++ server/routes/Upload.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+     
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, file.originalname.split('.')[0] + '-' + uniqueSuffix + '.' + file.originalname.split('.')[1]);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 
 const { uploadController } = require('../controllers/uploadController');
 
